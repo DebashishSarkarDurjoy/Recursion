d6ec8b28fc2ea43fe868264a12fb2b4addcc7744
DebashishSarkarDurjoy <dsdjoy4@gmail.com>
2022-04-26 11:49:04 +1000

    smart keypad practice 2

---
+++ keypad2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+string keypad[] = {"", "", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
+
+void keypadOutput(string input, string output, int i) {
+    // base case
+    if (input[i] == '\0') {
+        cout << output << endl;
+    }
+
+    // recursive case
+    int current_digit = input[i] - '0';
+    if (current_digit == 0 || current_digit == 1) {
+        keypadOutput(input, output, i+1);
+    }
+
+    for (int k = 0; k < keypad[current_digit].size(); k++) {
+        keypadOutput(input, output + keypad[current_digit][k], i+1);
+    }
+
+    return ;
+}
+
+int main(void) {
+    string input;
+    cin >> input;
+
+    string output;
+    keypadOutput(input, output, 0);
+
+    return 0;
+}
